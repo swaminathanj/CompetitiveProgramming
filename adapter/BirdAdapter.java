e469ad5d896e7ac153035781de5a709ab7a225ed
Swaminathan J <swaminathanj@yahoo.com>
2022-07-18 20:14:46 +0530

    Add files via upload

---
+++ adapter/BirdAdapter.java
@@ -0,0 +1,16 @@
+
+public class BirdAdapter implements ToyDuck {
+
+
+    Bird bird;
+    public BirdAdapter(Bird bird){
+        this.bird = bird;
+    }
+
+    @Override
+    public void squeak() {
+        bird.makeSound();
+    }
+
+
+}
