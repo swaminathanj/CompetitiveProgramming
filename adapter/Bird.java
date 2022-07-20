a93a28c29aa35eebd17c324cd91f4706d00af4f5
Swaminathan J <swaminathanj@yahoo.com>
2022-07-20 11:05:01 +0530

    Delete Bird.java

--- adapter/Bird.java
+++
@@ -1,8 +0,0 @@
-
-public interface Bird {
-	 // birds implement Bird interface that allows
-    // them to fly and make sounds adaptee interface
-    public void fly();
-    public void makeSound();
-
-}
