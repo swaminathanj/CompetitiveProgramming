46144b12a23ab7544e0cd80330350e0e822f9ab4
Aswathymohan <49934409+aswathyvaiga123@users.noreply.github.com>
2022-07-23 12:44:27 +0530

    Add files via upload

---
+++ builder/ConstructionEngineer.java
@@ -0,0 +1,16 @@
+
+public class ConstructionEngineer {
+	 private HouseBuilder houseBuilder;
+	    public ConstructionEngineer(HouseBuilder houseBuilder){
+	        this.houseBuilder = houseBuilder;
+	    }
+	    public House constructHouse() {
+	        this.houseBuilder.buildFoundation();
+	        this.houseBuilder.buildStructure();
+	        this.houseBuilder.buildRoof();
+	        this.houseBuilder.paintHouse();
+	        this.houseBuilder.furnishHouse();
+	        return this.houseBuilder.getHouse();
+	    }
+
+}
