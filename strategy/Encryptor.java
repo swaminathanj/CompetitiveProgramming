8f1ad7bd503755a3a3f6a3fc03d2c7efcdfccb83
Aswathymohan <49934409+aswathyvaiga123@users.noreply.github.com>
2022-07-26 18:48:53 +0530

    Add files via upload

---
+++ strategy/Encryptor.java
@@ -0,0 +1,16 @@
+public class Encryptor {
+	private EncryptionStrategy strategy;
+    private String plainText;
+    public Encryptor(EncryptionStrategy strategy){
+        this.strategy=strategy;
+    }
+    public void encrypt(){
+        strategy.encryptData(plainText);
+    }
+    public String getPlainText() {
+        return plainText;
+    }
+    public void setPlainText(String plainText) {
+        this.plainText = plainText;
+    }
+}
